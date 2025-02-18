dc1cd36262e066088f49689d8f1fa3beda5df06b
Mateusz Wala <mateusz.wala2734@gmail.com>
2025-02-18 22:01:08 +0100

    chore: Configure dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  reactStrictMode: false,
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  images: { unoptimized: true },
+};
 
 export default nextConfig;
