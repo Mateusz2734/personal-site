c54681650ec935f1b68a38de0f0c013306ff4fa5
Mateusz Wala <mateusz.wala2734@gmail.com>
2025-02-23 01:21:33 +0100

    feat: Add gradient animation and new GradientCard component

---
+++ src/components/cards.tsx
@@ -0,0 +1,10 @@
+export const GradientCard = () => {
+    return (
+        <div className="h-full rounded-lg opacity-50 animate-gradient"
+            style={{
+                background: "linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)",
+                backgroundSize: "400% 400%",
+            }}
+        />
+    );
+};
